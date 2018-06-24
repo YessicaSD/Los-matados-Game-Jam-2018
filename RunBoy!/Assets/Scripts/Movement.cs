e8fd9848e15261f011330aedb5e83c1b1bb9e919
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-06-24 14:05:35 +0200

    added project

---
+++ RunBoy!/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float sideSpeed;
+    public float forwardSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(0, 0, forwardSpeed * Time.deltaTime);
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sideSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+	}
+}
