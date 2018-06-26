28687ed3983ce9941e0e33588e7462d12c82634d
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-06-26 20:20:09 +0200

    score and win done

--- RunBoy!/Assets/Scripts/CircularMovementControll.cs
+++ RunBoy!/Assets/Scripts/CircularMovementControll.cs
@@ -16,41 +16,55 @@ public class CircularMovementControll : MonoBehaviour {
     int energyInt;
     //Ui
     public Text EnergyText;
-
+    public Text ScoreText;
+    public GameObject winText;
+    int score;
+    bool win;
     private void Start()
     {
         energy = 100;
         angle = ((3 * Mathf.PI) / 2);
         SetUiText();
+        score = 0;
+        win = false;
+        winText.SetActive(false);
     }
     // Update is called once per frame
     void FixedUpdate () {
-        angle += Input.GetAxis("Horizontal")* Time.deltaTime*sideSpeed;
-        if(energy>-100 && forwardSpeed>=0)
-        {
-            position += forwardSpeed;
-        }
-        
-        if(energy<=0.0f)
-        {
-            forwardSpeed -= 0.1f;
-        }
-        if(energy>0.0f)
-        {
-            energy -= 0.1f;
-        }
-       
-        //angled += Input.GetAxis("Horizontal") * Time.deltaTime * sideSpeed;
 
-        transform.position = new Vector3 (Mathf.Cos(angle) * radio, Mathf.Sin(angle) * radio, position);
-        transform.eulerAngles= new Vector3(0, 0, (angle*180)/Mathf.PI);
-        //transform.rotation.SetEulerAngles(0, 0, angled);
-
-        //transform.Rotate(0, 0, angled);
-        SetUiText();
-        if(forwardSpeed<=0)
+        if (!win)
         {
-            Application.LoadLevel(Application.loadedLevel);
+            angle += Input.GetAxis("Horizontal") * Time.deltaTime * sideSpeed;
+            if (energy > -100 && forwardSpeed >= 0)
+            {
+                position += forwardSpeed;
+            }
+
+            if (energy <= 0.0f)
+            {
+                forwardSpeed -= 0.1f;
+            }
+            if (energy > 0.0f)
+            {
+                energy -= 0.2f;
+            }
+            transform.position = new Vector3(Mathf.Cos(angle) * radio, Mathf.Sin(angle) * radio, position);
+            transform.eulerAngles = new Vector3(0, 0, (angle * 180) / Mathf.PI);
+            if (forwardSpeed <= 0)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+            //UI
+            score += 1;
+            SetUiText();
+            if(transform.position.z>=600)
+            {
+                win = true;
+            }
+        }
+       else
+        {
+            winText.SetActive(true);
         }
     }
     void OnCollisionEnter(Collision collision)
@@ -77,5 +91,6 @@ public class CircularMovementControll : MonoBehaviour {
     {
         energyInt = (int)energy;
         EnergyText.text = "Energy:" +  energyInt.ToString();
+       ScoreText.text = "Score:" + score.ToString();
     }
 }
